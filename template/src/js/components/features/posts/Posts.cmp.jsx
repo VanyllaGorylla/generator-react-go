670f5c02ec0450339a9a53b29b86ab6f010f3f93
Artur Perczak <artur.perczak@gmail.com>
2018-01-01 19:43:06 +0100

    Solving windows issue 2

---
+++ template/src/js/components/features/posts/Posts.cmp.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getPosts, deletePost, updatePost } from './posts.actions';
+
+import LoadingWrapper from '../../common/LoadingWrapper.cmp.jsx';
+import PostsList from './PostsList.cmp.jsx';
+
+class PostsCmp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+
+		this.onDeletePostClick = this.onDeletePostClick.bind(this);
+	}
+
+	componentDidMount() {
+		this.props.getPosts();
+	}
+
+	onDeletePostClick(e) {
+		e.preventDefault();
+		this.props.deletePost(parseInt(e.target.getAttribute('data-id')));
+	}
+
+	render() {
+		let { data, dataLoading } = this.props.posts;
+
+		return (
+			<div>
+				<div className="row">
+					<div className="col s12 m12 l12">
+						<div className="card">
+							<div className="card-content">
+								<span className="card-title">Posts</span>
+								<LoadingWrapper isLoading={dataLoading}>
+									<PostsList items={data}
+										onDeletePostClick={this.onDeletePostClick}
+									/>
+								</LoadingWrapper>
+								<div className="card-action">
+									<a href="/posts/new">Create new</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => ({
+	posts: state.posts
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	getPosts: () => dispatch(getPosts()),
+	deletePost: id => dispatch(deletePost(id)),
+	updatePost: post => dispatch(updatePost(post)),
+});
+
+export { PostsCmp };
+export default connect(mapStateToProps, mapDispatchToProps)(PostsCmp);
---
+++ template/test-src/js/components/features/todo-example/TodoExample.cmp.spec.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { shallow, mount, render, configure } from 'enzyme';
+import configureStore from 'redux-mock-store'
+
+import Adapter from 'enzyme-adapter-react-16';
+
+import WrapperTodoExample, { TodoExample } from '../../../../../src/js/components/features/todo-example/TodoExample.cmp.jsx';
+
+configure({ adapter: new Adapter() });
+
+
+describe('TodoExample test', function () {
+	let store, wrapper;
+
+	beforeEach(() => {
+		store = configureStore()({
+			todos: [
+				{
+					id: 1,
+					title: 'Do Laundry',
+					completed: false
+				},
+				{
+					id: 2,
+					title: 'Wash your dishes',
+					completed: true
+				},
+				{
+					id: 3,
+					title: 'Wash the floor',
+					completed: false
+				}
+			]
+		})
+	});
+
+	it('should show me html', function () {
+		const wrapper = render(
+			<WrapperTodoExample store={store} />
+		);
+		let ulLi = wrapper.find('ul li');
+		expect(ulLi.length).toEqual(3);
+	});
+
+	it('should start addTodo method', function () {
+		let wrapper, instance, input;
+		let ulLi;
+		let todos = [];
+		const addTodo = jest.fn();
+
+		// const spyHandleClick = spyOn(TodoExample.prototype, 'handleClick');
+		wrapper = mount(
+			<TodoExample todos={todos} addTodo={addTodo} />
+		);
+
+		wrapper.find('input').simulate('change', { target: { value: 'Arczik!' } })
+		wrapper.find('button').simulate('click');
+		expect(addTodo).toBeCalledWith('Arczik!');
+	});
+}); 
